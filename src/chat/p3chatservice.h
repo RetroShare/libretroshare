377d344e6830a99dde055cf4f80f8b11d1523b3d
jolavillette <jolavillette@gmail.com>
2026-02-28 10:36:25 +0100

    Add lobby history retrieval protocol: probe/request/data items, chunked transfer, local dedup & merge

--- src/chat/p3chatservice.h
+++ src/chat/p3chatservice.h
@@ -138,6 +138,8 @@ public:
     virtual void getDefaultIdentityForChatLobby(RsGxsId& nick_name) override;
     virtual void setLobbyAutoSubscribe(const ChatLobbyId& lobby_id, const bool autoSubscribe) override;
     virtual bool getLobbyAutoSubscribe(const ChatLobbyId& lobby_id) override;
+    virtual bool requestLobbyHistory(const ChatLobbyId& lobby_id) override;
+    virtual bool requestLobbyHistoryFromPeer(const ChatLobbyId& lobby_id, const RsPeerId& peer_id, uint32_t max_count, uint32_t oldest_ts) override;
 
     /** methods that will call the DistantChatService parent
      */
